cb856bef33decf8d2c5853f05bb1c581f8ecc8f0
Bigkeat <151060133+YenKeat@users.noreply.github.com>
2024-07-14 23:04:52 +0800

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,37 @@
+# include <iostream>
+# include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	int count = 1;
+
+	cout << endl;
+
+	for (int j = 0; j < 9; j++)
+	{
+		for (int i = 0; i < 5-count; i++)    // print space
+		{
+			cout << " ";
+		}
+		
+		for (int k = 0; k < ((count-1)*2+1); k++)    // print Asterisks
+		{
+			cout << "*";
+		}
+
+		if (j < 4)
+		{
+			count += 1;
+		}
+
+		if (j >= 4)
+		{
+			count -= 1;
+		}
+
+		cout << endl;
+	}
+	
+}
